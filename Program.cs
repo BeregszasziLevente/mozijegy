f95185bb5bb3e10d9ddbf7be23d48df91f28864d
BeregszasziLevente <beregszaszi.levente.2024d@telekimezotur.hu>
2026-09-16 12:11:26 +0200

    2. feladat

--- Program.cs
+++ Program.cs
@@ -1,1 +1,28 @@
-﻿Console.WriteLine("Hello, World!");
+﻿Console.Write("Add meg a neved: ");
+string nev = Console.ReadLine();
+
+Console.Write("Add meg a korod: ");
+int kor = int.Parse(Console.ReadLine());
+
+Console.Write("Van diákigazolványod? (igen/nem): ");
+string diakIgazolvany = Console.ReadLine();
+bool vanDiak = false;
+if (diakIgazolvany=="igen")
+{
+    vanDiak=true;
+} else if (diakIgazolvany=="nem")
+{
+    vanDiak = false;
+}
+
+Console.Write("A mozijegy ára: ");
+int jegyar=int.Parse(Console.ReadLine());
+
+Console.Write("Vásárolt popcorn darabszáma: ");
+int popcorn=int.Parse(Console.ReadLine());
+
+Console.Write("A popcorn egységára: ");
+int popcornAr=int.Parse(Console.ReadLine());
+
+double kedvezmeny = 0.0;
+
